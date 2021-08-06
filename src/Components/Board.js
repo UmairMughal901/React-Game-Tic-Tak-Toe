afe5f71905bae60c3afa2d10fb98774892536fbb
Umair Asghar <umairasghar.mughal@gmail.com>
2021-08-06 22:35:20 +0500

    Board.js

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import Square from "./Square";
 
-class Board extends React.Component{
-    renderBox(i){
-        return <Square value={i}/>
-    }
-    render(){
-        const status = 'Next player: X';
-        return(
-            <div>
+
+
+class Board extends React.Component {
+  understandBox(i) {
+    return <Square value={i} />
+  }
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
         <div className="status">{status}</div>
         <div className="board-row">
-          {this.renderBox(0)}
-          {this.renderBox(1)}
-          {this.renderBox(2)}
+          {this.understandBox(0)}
+          {this.understandBox(1)}
+          {this.understandBox(2)}
         </div>
         <div className="board-row">
-          {this.renderBox(3)}
-          {this.renderBox(4)}
-          {this.renderBox(5)}
+          {this.understandBox(3)}
+          {this.understandBox(4)}
+          {this.understandBox(5)}
         </div>
         <div className="board-row">
-          {this.renderBox(6)}
-          {this.renderBox(7)}
-          {this.renderBox(8)}
+          {this.understandBox(6)}
+          {this.understandBox(7)}
+          {this.understandBox(8)}
         </div>
       </div>
-        );
-    }
+    );
+  }
 }
 
 export default Board;
